fb36a66a362e1e6bc72fc23610f7f98345522499
Christopher N. KATOYI <me@christopher2k.dev>
2020-10-01 18:32:40 +0200

    feat: canvas

--- src/components/D3WithCanvas.tsx
+++ src/components/D3WithCanvas.tsx
@@ -1,51 +1,69 @@
-import React from "react";
-// import React, { useEffect, useRef } from "react";
-// import { scaleLinear } from "d3-scale";
+import React, { useEffect, useRef } from "react";
+import { extent } from "d3";
+import { NumberValue, scaleLinear } from "d3-scale";
+import { ChartProps, Dot } from "../types";
 
-import { ChartProps } from "../types";
+const D3WithCanvas: React.FC<ChartProps> = ({
+  data,
+  selectedClusters,
+  markerX,
+  markerY,
+  clusterColors,
+}) => {
+  const w = 300;
+  const h = 300;
 
-const D3WithCanvas: React.FC<ChartProps> = () => {
-  // const w = 300;
-  // const h = 300;
+  const canvasRef = useRef<HTMLCanvasElement>(null);
 
-  // const canvasRef = useRef<HTMLCanvasElement>(null);
+  let dots: Array<Dot> = [];
 
-  // const slicedData = rawData.slice(0, numberOfGroups);
+  for (let key in data) {
+    if (selectedClusters.includes(key)) {
+      dots.push(
+        ...data[key].map((d) => ({
+          x: d[markerX] as number,
+          y: d[markerY] as number,
+          color: clusterColors[key],
+        }))
+      );
+    }
+  }
 
-  // const xScale = scaleLinear().domain(xExtent).range([0, w]);
-  // const yScale = scaleLinear().domain(yExtent).range([h, 0]);
+  const xScale = scaleLinear()
+    .domain(extent(dots, (d) => d.x) as NumberValue[])
+    .range([0, w]);
+  const yScale = scaleLinear()
+    .domain(extent(dots, (d) => d.y) as NumberValue[])
+    .range([h, 0]);
 
-  // useEffect(() => {
-  //   if (canvasRef.current) {
-  //     const ctx = canvasRef.current.getContext("2d");
-  //     if (ctx != null) {
-  //       ctx.fillStyle = "#fff";
-  //       ctx.fillRect(0, 0, w, h);
-  //       for (const dotsData of slicedData) {
-  //         for (const d of dotsData.data) {
-  //           ctx.fillStyle = dotsData.color;
-  //           ctx.beginPath();
-  //           ctx.arc(
-  //             xScale(d.x) as number,
-  //             yScale(d.y) as number,
-  //             2,
-  //             0,
-  //             Math.PI * 2
-  //           );
-  //           ctx.stroke();
-  //           ctx.fill();
-  //         }
-  //       }
-  //     }
-  //   }
-  // }, [slicedData, xScale, yScale]);
+  useEffect(() => {
+    if (canvasRef.current) {
+      const ctx = canvasRef.current.getContext("2d");
+      if (ctx != null) {
+        ctx.fillStyle = "#fff";
+        ctx.fillRect(0, 0, w, h);
+        for (const dot of dots) {
+          ctx.fillStyle = dot.color;
+          ctx.beginPath();
+          ctx.arc(
+            xScale(dot.x) as number,
+            yScale(dot.y) as number,
+            1,
+            0,
+            Math.PI * 2
+          );
+          ctx.stroke();
+          ctx.fill();
+        }
+      }
+    }
+  }, [dots, xScale, yScale]);
 
-  // return (
-  //   <div>
-  //     <canvas ref={canvasRef} width={w} height={h}></canvas>
-  //   </div>
-  // );
-  return null;
+  return (
+    <div>
+      <canvas ref={canvasRef} width={w} height={h}></canvas>
+    </div>
+  );
 };
 
 export default D3WithCanvas;
